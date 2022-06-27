5ad8144bace0ea3cc9a79c65189433784dd11334
kkkdan <kkkdan@nate.com>
2022-06-27 11:09:06 +0900

    test 01

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React test 2022/06/27
         </a>
       </header>
     </div>
